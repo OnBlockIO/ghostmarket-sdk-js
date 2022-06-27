7f067a13ec598224121b35eff51a78f3eab6dd73
Vincent Geneste <vincentgeneste@gmail.com>
2022-06-27 12:56:52 -0700

    misc linting

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 // global`module` is undefined
 module.exports = {
-  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+    presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
 }
--- jest.config.ts
+++ jest.config.ts
@@ -1,38 +1,38 @@
 import { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  verbose: true,
-  automock: false,
-  clearMocks: true,
-  collectCoverage: true,
-  coverageProvider: 'babel',
-  // https://github.com/jest-community/jest-extended: Extended matchers
-  setupFilesAfterEnv: ['jest-extended/all', './setEnvVar'],
-  roots: ['src/__tests__'],
-  coveragePathIgnorePatterns: [
-    'node_modules/',
-    'src/abis/',
-    'src/common/',
-    'src/core/constants',
-    'src/external',
-    'src/types',
-    'src/utils',
-  ],
-  testPathIgnorePatterns: [
-    'node_modules/',
-    'src/abis/',
-    'src/common/',
-    'src/core/constants',
-    'src/external',
-    'src/types',
-    'src/utils',
-  ],
-  transformIgnorePatterns: ['node_modules/(?!(level-sublevel/node_modules/lgtgt)/)'],
-  transform: {
-    '\\.[jt]s?$': 'babel-jest',
-  },
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    verbose: true,
+    automock: false,
+    clearMocks: true,
+    collectCoverage: true,
+    coverageProvider: 'babel',
+    // https://github.com/jest-community/jest-extended: Extended matchers
+    setupFilesAfterEnv: ['jest-extended/all', './setEnvVar'],
+    roots: ['src/__tests__'],
+    coveragePathIgnorePatterns: [
+        'node_modules/',
+        'src/abis/',
+        'src/common/',
+        'src/core/constants',
+        'src/external',
+        'src/types',
+        'src/utils',
+    ],
+    testPathIgnorePatterns: [
+        'node_modules/',
+        'src/abis/',
+        'src/common/',
+        'src/core/constants',
+        'src/external',
+        'src/types',
+        'src/utils',
+    ],
+    transformIgnorePatterns: ['node_modules/(?!(level-sublevel/node_modules/lgtgt)/)'],
+    transform: {
+        '\\.[jt]s?$': 'babel-jest',
+    },
 }
 
 export default config
