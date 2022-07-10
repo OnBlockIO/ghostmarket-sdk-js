87059c20373b1080b260755498d569632bba327c
vincentgeneste <vincentgeneste@gmail.com>
2022-07-10 14:59:12 -0700

    added incentives ABI + contract hashes + MAX_INT_255

---
+++ src/abis/Incentives.ts
@@ -0,0 +1,4 @@
+import { PartialReadonlyContractAbi } from '../types/network'
+import IncentivesAbi from './IncentivesAbi.json'
+
+export const IncentivesContract = IncentivesAbi as PartialReadonlyContractAbi
