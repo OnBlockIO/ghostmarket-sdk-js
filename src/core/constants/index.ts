96b56a13ca4167a60cda8420592c99e0fe766f8f
vincentgeneste <vincentgeneste@gmail.com>
2022-08-04 14:21:52 -0700

    added lp staking constant (testnet only for now)

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -15,8 +15,8 @@ export interface ContractAddresses {
     EXCHANGE: string
     INCENTIVES: string
     GM_TOKEN: string
-    STAKING_LP?: string
-    STAKING_SINGLE?: string
+    LP_TOKEN?: string
+    LP_STAKING?: string
     ROYALTIES?: string
     PROXY_NFT?: string
     PROXY_ERC20?: string
@@ -37,7 +37,8 @@ export const ETHEREUM_MAINNET_ADDRESSES: ContractAddresses = {
     PROXY_NFT: '0x1bb6C21e6adB8757F46e77A7F4c5Ad9118f4A04d',
     PROXY_ERC20: '0x153909fB5232c72B5E613aae4898291b014785a1',
     WRAPPED_TOKEN: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-    STAKING_LP: '',
+    LP_TOKEN: '',
+    LP_STAKING: '',
 }
 
 export const ETHEREUM_TESTNET_ADDRESSES: ContractAddresses = {
@@ -50,7 +51,8 @@ export const ETHEREUM_TESTNET_ADDRESSES: ContractAddresses = {
     WRAPPED_TOKEN: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
     GHOST_ERC721: '0xd35b5d7e184013233cc43139dc7242223ec0a708',
     GHOST_ERC1155: '0x76ffb975ca6c3f0f9e036561824c542374885e95',
-    STAKING_LP: '',
+    LP_TOKEN: '',
+    LP_STAKING: '',
 }
 
 export const AVALANCHE_MAINNET_ADDRESSES: ContractAddresses = {
@@ -63,7 +65,8 @@ export const AVALANCHE_MAINNET_ADDRESSES: ContractAddresses = {
     WRAPPED_TOKEN: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
     GHOST_ERC721: '0x068bEF92987D16eF682FF017B822CA1211401EaF',
     GHOST_ERC1155: '0xdcdaB251151c345AD527851ECa783521Ea3209E0',
-    STAKING_LP: '',
+    LP_TOKEN: '',
+    LP_STAKING: '',
 }
 
 export const AVALANCHE_TESTNET_ADDRESSES: ContractAddresses = {
@@ -76,7 +79,8 @@ export const AVALANCHE_TESTNET_ADDRESSES: ContractAddresses = {
     WRAPPED_TOKEN: '0xd00ae08403B9bbb9124bB305C09058E32C39A48c',
     GHOST_ERC721: '0x31681e95A89034612926908F48A5E1Aa734EBf05',
     GHOST_ERC1155: '0xE98E9D752d6104aDa0520988cd1834035762C8c7',
-    STAKING_LP: '',
+    LP_TOKEN: '',
+    LP_STAKING: '',
 }
 
 export const POLYGON_MAINNET_ADDRESSES: ContractAddresses = {
@@ -89,7 +93,8 @@ export const POLYGON_MAINNET_ADDRESSES: ContractAddresses = {
     WRAPPED_TOKEN: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
     GHOST_ERC721: '0x068bef92987d16ef682ff017b822ca1211401eaf',
     GHOST_ERC1155: '0xf1c82f5ddb4f1a6a8f3eed2eb25fc39fc6d33fb3',
-    STAKING_LP: '',
+    LP_TOKEN: '',
+    LP_STAKING: '',
 }
 
 export const POLYGON_TESTNET_ADDRESSES: ContractAddresses = {
@@ -102,7 +107,8 @@ export const POLYGON_TESTNET_ADDRESSES: ContractAddresses = {
     WRAPPED_TOKEN: '0x9c3c9283d3e44854697cd22d3faa240cfb032889',
     GHOST_ERC721: '0x9b7e1a760751de8251e9f1ad09ed9039d4b7a676',
     GHOST_ERC1155: '0x7aa199E2D5cFf1E6275A33c8dCE3c6085E393781',
-    STAKING_LP: '',
+    LP_TOKEN: '',
+    LP_STAKING: '',
 }
 
 export const BSC_MAINNET_ADDRESSES: ContractAddresses = {
@@ -115,7 +121,8 @@ export const BSC_MAINNET_ADDRESSES: ContractAddresses = {
     WRAPPED_TOKEN: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
     GHOST_ERC721: '0xF41db445D7eaF45536985cE185ce131FA4b42E68',
     GHOST_ERC1155: '0x44c5ce28c29934b71a2a0447745d551dfc7b5133',
-    STAKING_LP: '',
+    LP_TOKEN: '',
+    LP_STAKING: '',
 }
 
 export const BSC_TESTNET_ADDRESSES: ContractAddresses = {
@@ -128,7 +135,8 @@ export const BSC_TESTNET_ADDRESSES: ContractAddresses = {
     WRAPPED_TOKEN: '0x094616f0bdfb0b526bd735bf66eca0ad254ca81f',
     GHOST_ERC721: '0x92832367C614A9e36D4d6394f0DA44306DF2D4D7',
     GHOST_ERC1155: '0xf1daF0C96d13251dE0FdbB60acD2caD3C9CF15BB',
-    STAKING_LP: '',
+    LP_TOKEN: '0x179b6b41cd9ba207ccfd77fbb4c232dd4962a9eb',
+    LP_STAKING: '0x9395c79A9074F8DB64A815bF19133E7959c3DAd6',
 }
 
 export const N3_MAINNET_ADDRESSES: ContractAddresses = {
